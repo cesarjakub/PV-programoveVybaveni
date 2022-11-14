4979e9061d4f541211665909ba17b4ae9e0dcbc7
Jakub César <84319967+cesarjakub@users.noreply.github.com>
2022-11-14 08:14:40 +0100

    myStack-linkedList

---
+++ stack-linkedlist/stack/stack/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> sta = new MyStack<int>();
+            sta.Push(5);
+            sta.Push(2);
+            sta.Push(3);
+            sta.Push(4);
+
+
+            while (!sta.IsEmpty())
+            {
+                Console.WriteLine(sta.Pop());
+            }
+        }
+    }
+}
